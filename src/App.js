961b681973e0517bf30e0cc10464226241a70fda
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2022-11-14 19:48:29 -0700

    updated readme and removed unused lines

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import { useState, useEffect} from 'react';
 import RecipeContainer from './components/RecipeContainer/RecipeContainer';
-import RecipeLeft from './components/RecipeLeft/RecipeLeft';
-import RecipeRight from './components/RecipeRight/RecipeRight';
 const url = "https://636533ed046eddf1bae83cdf.mockapi.io/api/recipes/recipes";
 
-const api = 'https://636533ed046eddf1bae83cdf.mockapi.io/api/recipes/:endpoint';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -24,6 +21,7 @@ function App() {
 
   const recipeComponents = recipes.map( r => (
     <RecipeContainer
+      key={r.id}
       recipe={r}
     />
   ));
